9853e39a75ba4055c9aae535c7f28744e4364f7c
sissy <sissytseng@gmail.com>
2015-04-26 14:14:38 +0800

    簡單模型繫結與複雜模型繫結

---
+++ MVC5Course/Models/Simple1ViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC5Course.Models
+{
+    public class Simple1ViewModel:IValidatableObject
+    {
+        [Required]
+        public string  Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            yield break;
+        }
+    }
+}
