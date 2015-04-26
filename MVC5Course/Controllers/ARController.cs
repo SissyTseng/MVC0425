5fc9937a3455a7a735b7c85e9e002495873af52d
sissy <sissytseng@gmail.com>
2015-04-26 10:31:57 +0800

    JavaScriptResult 與 JsonResult 的使用

--- MVC5Course/Controllers/ARController.cs
+++ MVC5Course/Controllers/ARController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -27,6 +28,33 @@ namespace MVC5Course.Controllers
         public string Content1()
         {
             return "Disallow: /";
+        }
+
+        public ActionResult File1()
+        {
+            var wc = new WebClient();
+            var content = wc.DownloadData("http://static.ettoday.net/images/5/d5975.jpg");
+            return File(content, "image/jpg","Img1.jpg");
+        }
+
+
+        public ActionResult File4()
+        {
+            return File(Server.MapPath("~/Content/File1.html"), "text/html");
+        }
+
+        public ActionResult File5()
+        {
+            byte[] content = System.IO.File.ReadAllBytes(Server.MapPath("~/Content/Img1.jpg"));
+
+            return File(content, "image/jpg", "File5.png");
+        }
+        public ActionResult Json1()
+        {
+            db.Configuration.LazyLoadingEnabled = false;
+            var data = db.Product.Take(10);
+
+            return Json(data, JsonRequestBehavior.AllowGet);
         }
     }
 }
