15da00119ddad1caba58aa949f1d1c7b6c73377b
sissy <sissytseng@gmail.com>
2015-05-09 11:29:21 +0800

    Razor 練習

--- MVC5Course/Controllers/BaseController.cs
+++ MVC5Course/Controllers/BaseController.cs
@@ -1,4 +1,5 @@
-﻿using MVC5Course.Models;
+﻿using MVC5Course.ActionFilters;
+using MVC5Course.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +8,8 @@ using System.Web.Mvc;
 
 namespace MVC5Course.Controllers
 {
+
+    [Logger]
     public abstract class BaseController : Controller
     {
         protected FabricsEntities db = new FabricsEntities();
