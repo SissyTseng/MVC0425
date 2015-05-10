75e5832e61a4adff067af7b6aff634f6fbb4d1fd
sissy <sissytseng@gmail.com>
2015-05-10 17:28:30 +0800

    Enum

---
+++ MVC5Course/Models/RegViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC5Course.Models
+{
+    public enum MemberLevel
+    {
+        User,
+        Admin
+    }
+
+    public class RegViewModel
+    {
+        public string UserID { get; set; }
+        public string UserPW { get; set; }
+
+        public MemberLevel Level { get; set; }
+    }
+}
